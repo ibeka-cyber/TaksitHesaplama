ce5d39547cb1e93f4bee28a47530133b4b853c85
Bahar <irembahar4118@gmail.com>
2023-01-17 01:48:11 +0300

    taksit hesaplama
    c#

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürünün Fiyatını Giriniz   : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ödeme nasıl yapılacak : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kaç taksit istersiniz      : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fatura için ad-soyad    : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefon numarası        : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(58, 266);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nakit",
+            "Halkbank",
+            "Vakıfbank",
+            "Ziraatbank",
+            "Diğer Kartlar"});
+            this.comboBox1.Location = new System.Drawing.Point(184, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Enter += new System.EventHandler(this.comboBox1_Enter);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            this.comboBox1.Leave += new System.EventHandler(this.comboBox1_Leave);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Tek Çekim",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox2.Location = new System.Drawing.Point(184, 132);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(141, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Enter += new System.EventHandler(this.comboBox2_Enter);
+            this.comboBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox2_KeyPress);
+            this.comboBox2.Leave += new System.EventHandler(this.comboBox2_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(184, 220);
+            this.maskedTextBox1.Mask = "(999) 000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(141, 20);
+            this.maskedTextBox1.TabIndex = 10;
+            this.maskedTextBox1.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.maskedTextBox1.Enter += new System.EventHandler(this.maskedTextBox1_Enter);
+            this.maskedTextBox1.Leave += new System.EventHandler(this.maskedTextBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 61);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "GÖSTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(356, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "PRINT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(356, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 61);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "DOSYAYA YAZ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 326);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label6.Text = DateTime.Now.ToLongTimeString();
+            ToolTip bilgilendirme = new ToolTip();
+            bilgilendirme.SetToolTip(comboBox1, "1- Halkbank (3), Vakıfbank (4) ve Ziraatbank (5) taksit seçildiğinde peşin fiyatına taksitlendirme yapılır.\n" +
+                                            "2 - Diğer taksit seçeneklerinde aylık % 2 faiz uygulanır.\n" +
+                                           "3 - Diğer kartlar ile aylık % 2 faiz uygulanır.\n" +
+                                            "4 - En fazla 6 taksit seçilebilir.\n" +
+                                           "5 - Nakit ve tek çekimde ürün fiyatı uygulanır.");
+            bilgilendirme.ToolTipTitle = "Bilgilendirme";
+        }
+        private void uyari()
+        {
+            MessageBox.Show("Bu alanı boş geçemezsiniz.", "UYARI");
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((char.IsDigit(e.KeyChar) == false) && (char.IsControl(e.KeyChar) == false))
+                e.KeyChar = '\0';
+        }
+        private void maskedTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(maskedTextBox1.Text))
+            {
+                uyari();
+                maskedTextBox1.Focus();
+            }
+            maskedTextBox1.BackColor = Color.White;
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Red;
+        }
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                e.KeyChar = '\0';
+        }
+        private void comboBox1_Enter(object sender, EventArgs e)
+        {
+            comboBox1.BackColor = Color.Red;
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                uyari();
+                textBox1.Focus();
+            }
+            textBox1.BackColor = Color.White;
+        }
+        private void comboBox1_Leave(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "")
+            {
+                uyari();
+                comboBox1.Focus();
+            }
+            comboBox1.BackColor = Color.White;
+        }
+        private void comboBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                e.KeyChar = '\0';
+        }
+        private void comboBox2_Enter(object sender, EventArgs e)
+        {
+            comboBox2.BackColor = Color.Red;
+        }
+        private void comboBox2_Leave(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "")
+            {
+                uyari();
+                comboBox2.Focus();
+            }
+            comboBox2.BackColor = Color.White;
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = "0123456789";
+            if (str.IndexOf(e.KeyChar) != -1)
+                e.KeyChar = '\0';
+        }
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.Red;
+        }
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                uyari();
+                textBox2.Focus();
+            }
+            textBox2.BackColor = Color.White;
+            textBox2.Text = textBox2.Text.ToUpper();
+        }
+        private void maskedTextBox1_Enter(object sender, EventArgs e)
+        {
+            maskedTextBox1.BackColor = Color.Red;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || comboBox1.Text == "" || comboBox2.Text == "" || textBox2.Text == "" || maskedTextBox1.Text == "")
+                MessageBox.Show("Formda boş bıraktığınız alanları doldurunuz.", "HATA");
+            else if (comboBox2.Text == "Tek Çekim")
+            {
+                MessageBox.Show("Seçiminiz      : " + comboBox1.Text + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" + "Tek çekim yaptığınız için borcunuz : " + textBox1.Text + "\n\n" + "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);
+            }
+            else
+            {
+                if (comboBox1.Text == "Nakit")
+                {
+                    MessageBox.Show("Seçiminiz      : Nakit" + "\n" + "Taksit Sayısı : " + comboBox2.Text + "\n" + "Nakit şeçimini yaptığınız için borcunuz : " + textBox1.Text + "\n\n" + "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);
+
+                }
+                if (comboBox1.Text == "Halkbank" && int.Parse(comboBox2.Text) <= 3)
+                {
+                    MessageBox.Show("Seçiminiz      : Halkbank" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuz aylık " + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                 
+                }
+                else if (comboBox1.Text == "Halkbank" && int.Parse(comboBox2.Text) > 3)
+                {
+                    MessageBox.Show("Seçiminiz      : Halkbank" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuz aylık " + (double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) * 0.02) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                
+                }
+                if (comboBox1.Text == "Vakıfbank" && int.Parse(comboBox2.Text) <= 4)
+                {
+                    MessageBox.Show("Seçiminiz      : Vakıfbank" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuz aylık " + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                
+                }
+                else if (comboBox1.Text == "Vakıfbank" && int.Parse(comboBox2.Text) > 4)
+                {
+                    MessageBox.Show("Seçiminiz      : Vakıfbank" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuzu aylık " + (double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) * 0.02) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                  
+                }
+                if (comboBox1.Text == "Ziraatbank" && int.Parse(comboBox2.Text) <= 5)
+                {
+                    MessageBox.Show("Seçiminiz      : Ziraat Bankası" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuz aylık " + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                 
+                }
+                else if (comboBox1.Text == "Ziraatbank" && int.Parse(comboBox2.Text) > 5)
+                {
+                    MessageBox.Show("Seçiminiz      : Ziraat Bankası" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuzu aylık " + (double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) * 0.02) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                
+                }
+                if (comboBox1.Text == "Diğer Kartlar")
+                {
+                    MessageBox.Show("Seçiminiz      : Diğer Kartlar" + "\n" + "Taksit Sayısı  : " + comboBox2.Text + "\n" +
+                        textBox1.Text + " tl olan borcunuz aylık " + (double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) + double.Parse(textBox1.Text) / double.Parse(comboBox2.Text) * 0.02) + " tl olarak ödeyebilirsiniz." + "\n\n" +
+                        "Tarih : " + dateTimePicker1.Text + "\n" + "Saat  : " + label6.Text);                
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                textBox1.Font = fontDialog1.Font;
+                textBox1.ForeColor = fontDialog1.Color;
+                textBox2.Font = fontDialog1.Font;
+                textBox2.ForeColor = fontDialog1.Color;
+                comboBox1.Font = fontDialog1.Font;
+                comboBox1.ForeColor = fontDialog1.Color;
+                comboBox2.Font = fontDialog1.Font;
+                comboBox2.ForeColor = fontDialog1.Color;
+                maskedTextBox1.Font = fontDialog1.Font;
+                maskedTextBox1.ForeColor = fontDialog1.Color;
+                dateTimePicker1.Font = fontDialog1.Font;
+                dateTimePicker1.ForeColor = fontDialog1.Color;
+                label6.Font = fontDialog1.Font;
+                label6.ForeColor = fontDialog1.Color;
+            }
+            PrintDialog prd = new PrintDialog();
+            {
+                PrintDocument göster = new PrintDocument();
+                DialogResult yazdirma;
+                yazdirma = prd.ShowDialog();
+                göster.PrintPage += Göster_PrintPage;
+
+                if (yazdirma == DialogResult.OK)
+                {
+                    göster.Print();
+                }
+            }
+        }
+        private void Göster_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            SolidBrush renk = new SolidBrush(fontDialog1.Color);
+            e.Graphics.DrawString("Ürün fiyatı : " + textBox1.Text, textBox1.Font, renk, 200, 20);
+            e.Graphics.DrawString("Ödeme : " + comboBox1.Text, comboBox1.Font, renk, 200, 60);
+            e.Graphics.DrawString("Taksit : " + comboBox2.Text, comboBox2.Font, renk, 200, 100);
+            e.Graphics.DrawString("Ad-Soyad : " + textBox2.Text, textBox2.Font, renk, 200, 140);
+            e.Graphics.DrawString("Telefon : " + maskedTextBox1.Text, maskedTextBox1.Font, renk, 200, 180);
+            e.Graphics.DrawString("Tarih : " + dateTimePicker1.Text, dateTimePicker1.Font, renk, 200, 220);
+            e.Graphics.DrawString("Saat : " + label6.Text, label6.Font, renk, 200, 260);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Title = "Kayıt Yerinizi Seçiniz";
+                saveFileDialog1.Filter = "(*.doc)|*.doc|(*.txt)|*.txt|Tüm dosyalar(*.*)|*.*";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.InitialDirectory = "C:\\";
+                saveFileDialog1.ShowDialog();
+                StreamWriter Kaydet = new StreamWriter(saveFileDialog1.FileName);
+                Kaydet.WriteLine(textBox2.Text);
+                Kaydet.Close();
+                MessageBox.Show("Kaynak metin belgesine yazdırıldı.","BİLGİ");
+            }
+            catch
+            {
+                MessageBox.Show("Kaynak metin belgesine yazdırılmadı.","BİLGİ");
+            }
+        }
+    }
+}
